cbcdb5b0ab033d81f38932a5a8d3b304bb088b56
ishumski <whalterwhite23@gmail.com>
2021-01-14 14:03:12 +0300

    done task 3'

---
+++ JS/DOM/5.work_with_events(lesson_23)/3.task/js/script.js
@@ -0,0 +1,10 @@
+/*3. Привяжите всем ссылкам событие - по наведению на ссылку в конец ее текста дописывается ее href в круглых скобках. */
+
+const arrOfLinks = document.querySelectorAll("a");
+
+arrOfLinks.forEach((elem) => {
+
+    elem.addEventListener("click", () => {
+        elem.innerHTML = `${elem.innerHTML + elem.href}`;
+    })
+})
