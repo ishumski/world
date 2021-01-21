7af95319111906066aad8586b84d4ef8b338d80a
ishumski <whalterwhite23@gmail.com>
2021-01-21 11:56:30 +0300

    Added 25 task

---
+++ JS/DOM/6.hi_lvl_work_with_DOM(lesson_24)/25.task/js/script.js
@@ -0,0 +1,5 @@
+/*25. Дан элемент #elem. Найдите его родителя и покрасьте его в красный цвет.parentElement, parentNode.*/
+
+const p = document.getElementById("elem").parentNode;
+
+p.style.backgroundColor = "red";
