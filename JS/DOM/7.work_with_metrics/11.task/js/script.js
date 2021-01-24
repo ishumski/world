6abac008f9edc7dd9fcfbca19be379e0dc2c79a8
ishumski <whalterwhite23@gmail.com>
2021-01-24 17:29:08 +0300

    Added new way

--- JS/DOM/7.work_with_metrics/11.task/js/script.js
+++
@@ -1,9 +0,0 @@
-/*11. Дан элемент #elem с вертикальной полосой прокрутки. 
-По нажатию на кнопку прокрутите его на 100px вниз от текущего положения. */
-
-const div = document.querySelector("#elem");
-const button = document.querySelector("button");
-
-button.addEventListener("click", () => {
-    div.scrollLeft = 100;
-})
--- JS/DOM/7.work_with_metrics/18.task/js/script.js
+++
@@ -1,8 +0,0 @@
-/*Дана страница с вертикальной полосой прокрутки. 
-По нажатию на кнопку прокрутите на 300px вниз от текущего положения. */
-
-const button = document.querySelector("button");
-
-button.addEventListener("click", () => {
-    window.scrollBy(0, 300);
-})
