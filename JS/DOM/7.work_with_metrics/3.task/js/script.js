89570d7fa9665c756eb47e2e35e6113391cecb83
ishumski <whalterwhite23@gmail.com>
2021-01-24 11:51:00 +0300

    Done task 3

---
+++ JS/DOM/7.work_with_metrics/3.task/js/script.js
@@ -0,0 +1,11 @@
+/*3. Дан элемент #elem с границами. По нажатию на кнопку выведите его полную ширину с учетом границы и padding*/
+
+const p = document.querySelector("#elem");
+const button = document.querySelector("button");
+
+p.style.border = "3px solid black";
+
+button.addEventListener("click", () => {
+    const width = p.clientWidth;
+    console.log(width);
+})
