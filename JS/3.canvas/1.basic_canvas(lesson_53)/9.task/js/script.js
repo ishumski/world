1e205eb16ecf0ca9231ef2635d212d08b7dd0284
ishumski <whalterwhite23@gmail.com>
2021-02-08 13:17:54 +0300

    Done task 9

---
+++ JS/3.canvas/1.basic_canvas(lesson_53)/9.task/js/script.js
@@ -0,0 +1,5 @@
+/*9.  Используя метод fillRect, нарисуйте на канвасе квадрат */
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillRect(50, 50, 100, 100);
