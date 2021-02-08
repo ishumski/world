4ac867296af30ebd83c108dab7b3bb840559cd5b
ishumski <whalterwhite23@gmail.com>
2021-02-08 14:34:00 +0300

    Done task 17

---
+++ JS/3.canvas/1.basic_canvas(lesson_53)/17.task/js/script.js
@@ -0,0 +1,8 @@
+/*17. Используя метод arc, нарисуйте на канвасе половину границы круга */
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.arc(150, 150, 100, Math.PI/180, Math.PI/1);
+// ctx.arc(150, 150, 100, 0.1, 3); если ы радианах
+ctx.closePath();
+ctx.stroke();
