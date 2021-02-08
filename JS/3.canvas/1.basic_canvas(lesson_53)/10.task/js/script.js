8c8e3f71faba648174c44cf5e2e502b005683710
ishumski <whalterwhite23@gmail.com>
2021-02-08 13:21:19 +0300

    Done task 10

---
+++ JS/3.canvas/1.basic_canvas(lesson_53)/10.task/js/script.js
@@ -0,0 +1,5 @@
+/*10.  Используя метод strokeRect, нарисуйте на канвасе квадрат */
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeRect(0, 0, 150, 150);
